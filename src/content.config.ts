d5dd336053eae13b77820e932e5e9eb39971256c
Vikram Soni <vikram9880@gmail.com>
2025-02-01 02:25:20 +0100

    blog final

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,6 +12,7 @@ const blog = defineCollection({
 		publishDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		tags: z.array(z.string()).optional(),
 	}),
 });
 
